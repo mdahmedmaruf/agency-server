cb985c6c23711daa0f10641e0f7630704236846b
mdahmedmaruf <mdahmedmaruf@gmail.com>
2020-10-16 22:44:11 +0600

    some edits

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const fs = require('fs-extra');
 const { query } = require('express');
 require('dotenv').config();
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.juysi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
@@ -17,11 +16,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('service-images'));
 app.use(fileUpload());
-
 const port = 4000;
-
 console.log(process.env.DB_USER);
-
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -30,12 +26,23 @@ client.connect(error => {
   const reviewCollections = client.db(process.config.DB_NAME).collection("Reviews");
   const serviceCollections = client.db(process.config.DB_NAME).collection("Services");
   const adminCollections = client.db(process.config.DB_NAME).collection("Admins");
-  
+
   app.post('/addNewOrder', (req, res) => {
-    const orderCollection = req.body;
-    orderCollections.insertOne(orderCollection)
+    const file = req.files.layoutDesign;
+    const name = req.body.name;
+    const email = req.body.email;
+    const orderCategory = req.body.orderCategory;
+    const details = req.body.details;
+    const price = req.body.price;
+    const newImg = file.data;
+    const encodeImg = newImg.toString('base64');
+    var image = {
+      contentType: file.minetype,
+      size: file.size,
+      img: Buffer.from(encodeImg, 'base64')
+    };
+    orderCollections.insertOne({name, email, orderCategory, details, price, image})
     .then(result => {
-      //console.log(result.insertedCount);
       res.send(result.insertedCount > 0);
     });
   });
@@ -74,18 +81,14 @@ client.connect(error => {
       res.send(result.insertedCount > 0);
     });
   });
-    //return res.send({name: file.name, path: `/${file.name}`})
-
-
 
   app.post('/isAdmin', (req, res) => {
     const email = req.body.email;
     adminCollections.find({email: email})
     .toArray((error, admins) => {
       res.send(admins.length > 0);
-    })
-  })
-
+    });
+  });
 
   app.get('/AllOrderLists', (req, res) => {
     orderCollections.find({})
@@ -94,14 +97,12 @@ client.connect(error => {
     });
   });
 
-
   app.get('/AllServices', (req, res) => {
     serviceCollections.find({})
     .toArray((error, documents) => {
       res.send(documents);
     });
   });
-
 
   app.get('/AllReviews', (req, res) => {
     reviewCollections.find({})
@@ -110,11 +111,6 @@ client.connect(error => {
     });
   });
 
-
-
-  
-
-  
 });
 
 
